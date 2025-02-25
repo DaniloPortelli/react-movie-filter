441735d82cd3653dc6bc4732237d4d70f98dfd6d
DaniloPortelli <daniloportelli@gmail.com>
2025-02-25 14:45:36 +0100

    inserimento form

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
-import { useState } from 'react'
-import film from "../data/film"
-// Create un nuovo progetto React e implementate un sistema di filtro per 
-// una lista di film in base al genere.
-
+// Create un nuovo progetto React e implementate un sistema di filtro per una lista di film in base al genere.
 // Dovrete utilizzare lo stato e useEffect per gestire il filtraggio dinamico.
-// Per oggi diamo priorità alla logica e alla gestione dello stato. Una volta 
-// funzionante, possiamo pensare allo stile!
+
 // Note
-// Il filtro deve funzionare dinamicamente quando l'utente seleziona un 
-// genere dalla select.
+// Il filtro deve funzionare dinamicamente quando l'utente seleziona un genere dalla select.
 // Se non viene selezionato alcun genere, devono essere mostrati tutti i film.
-// BONUS:
-// Aggiungere un campo di ricerca per filtrare i film anche per titolo.
-// Creare un sistema per aggiungere nuovi film alla lista tramite un form.
+
+import { useState } from 'react'
+import film from '../data/film'
 
 function App() {
 
-  const [movie, setMovie] = useState(film);
-  const [filteredMovie, setFilteredMovie] = useState("");
+ //Destrutturizzazione di useState: "movies"= array "film", 
+ // setMovies= funzione che mi permette di cambiare il valore di "movies"
+  const [movies, setMovies] = useState(film)
+
+
+  const [newMovie, setNewMovie] = useState("")
+
+
+  //Funzione da applicare a "onSubmit" che mi permette di impostare la logica di "setMovies"
+  // per cambiare il valore di "movies" ed impostarlo come un nuovo array contenente l'array
+  //stesso più il "newMovie"
+  const addMovie = (e) => {
+    e.preventDefault();
+    const movie = newMovie.trim()
+    setMovies([...movies, movie])
+    setNewMovie("")
+  }
 
   return (
     <>
-  <div>
-    prova
-  </div>
+      <div>
+        <h1>Elenco movies</h1>
+        <ul>
+          {
+            movies.map((movie, indice) => {
+
+              return (
+                <li key={indice}>{movie}</li>
+              )
+            })
+          }
+        </ul>
+
+        <form onSubmit={addMovie}>
+
+          {/* input di testo il cui value è "newMovie(vuoto)" */}
+          <input type="text"
+            value={newMovie}
+            // alla funzione onChange applico la funzione "setNewMovie"
+            //per definire "newMovie"
+            onChange={(e) => setNewMovie(e.target.value)}
+          />
+          <button>Invia</button>
+        </form>
+
+      </div>
     </>
   )
 }
 
 export default App
+
+ 
